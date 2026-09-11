c435c667d075cb81955ad9121f39a9608631c5e9
ahmadsaif12 <ahmads87237@gmail.com>
2026-09-11 22:51:29 +0545

    feat: add admin interfaces, forms, filters, signals, and tables for Profile and Vendor models

---
+++ accounts/forms.py
@@ -0,0 +1,92 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile, Customer, Vendor
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email'
+        ]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'telephone',
+            'email',
+            'first_name',
+            'last_name',
+            'profile_picture'
+        ]
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = [
+            'first_name',
+            'last_name',
+            'address',
+            'email',
+            'phone',
+            'loyalty_points'
+        ]
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter last name'
+            }),
+            'address': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter address',
+                'rows': 3
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter email'
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter phone number'
+            }),
+            'loyalty_points': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter loyalty points'
+            }),
+        }
+
+
+class VendorForm(forms.ModelForm):
+    class Meta:
+        model = Vendor
+        fields = ['name', 'phone_number', 'address']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Vendor Name'}
+            ),
+            'phone_number': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': 'Phone Number'}
+            ),
+            'address': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Address'}
+            ),
+        }
+
